50c74dd8460db931be5f09732d0704a40ef9d8a7
Mahesh Sathe <123986496+maheshsathe07@users.noreply.github.com>
2023-06-29 21:55:34 +0530

    Update Q1_Adder.java

--- Arrays/Assignments1/Q1_Adder.java
+++ Arrays/Assignments1/Q1_Adder.java
@@ -1,17 +1,10 @@
 import java.util.*;
-public class Adder {
-	int[] arr;
-	int targetSum ;
+class Q1_Adder {
+	static void numsums(int arr[], int targetSum) {
 
-	void getData(int[] arr) {
-		this.arr = arr;
-	}
-	
-	void numsums(int targetSum) {
-
-		for(int i=0; i<this.arr.length; i++) {
-			for(int j=i+1; j<this.arr.length; j++) {
-				if(this.arr[i] + this.arr[j] == targetSum) {
+		for(int i=0; i<arr.length; i++) {
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i] + arr[j] == targetSum) {
 					System.out.println("(" + i + ", "+j + ")");
 				}
 			}
@@ -21,6 +14,8 @@ public class Adder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Target sum : ");
+		int targetSum = sc.nextInt();
 		System.out.println("Enter Array Size");
 		int n = sc.nextInt();
 		int[] arr = new int[n];
@@ -29,10 +24,8 @@ public class Adder {
 			int el = sc.nextInt();
 			arr[i] = el;
 		}
-		Adder a = new Adder();
-		a.getData(arr);
-		System.out.println("Enter Target sum : ");
-		int targetSum = sc.nextInt();
-		a.numsums(targetSum);
+//		Q1_Adder a = new Q1_Adder();
+//		a.getData(arr);
+		numsums(arr, targetSum);
 	}
 }
